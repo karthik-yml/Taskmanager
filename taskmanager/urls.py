976fe155bfe5cbd4e9914caca5b97100233a43ce
karthik-yml <karthik.tv@ymedialabs.com>
2023-08-06 21:50:03 +0530

    Added Swagger Documentation

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -14,10 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task Manager",
+        default_version='v1',
+        description="These APIs can manage your Tasks",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('api/', include('your_app.urls')),  # Replace with your app's URLs
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/users/', include("users.urls")),
     path('api/tasklist/', include("tasklist.urls")),
     path('api/tasks/', include("tasks.urls"))
